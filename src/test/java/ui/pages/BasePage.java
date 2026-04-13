2c7ecf7d897d92d9608d918dae4eb1b6100be0d2
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2026-04-12 21:24:15 -0700

    Add BasePage and BaseTest classes

---
+++ src/test/java/ui/pages/BasePage.java
@@ -0,0 +1,12 @@
+package ui.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
